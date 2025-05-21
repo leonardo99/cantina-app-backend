941df8d5c1f75420d0af7fe3da193f244b92bc38
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-21 20:11:57 -0300

    Valida dados antes de registrar usuário

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:2|max:255',
+            'email' => 'required|unique:users|email',
+            'type' => 'required|in:student,responsible,admin',
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()],
+        ];
+    }
+}

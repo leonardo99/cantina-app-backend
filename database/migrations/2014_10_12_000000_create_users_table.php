a393bcff1bdec085420404a6f013a1a6ad19e3eb
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-21 20:12:53 -0300

    Adiciona os campos type e phone na tabela de usuários

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 14)->nullable();
             $table->enum('type', ['student', 'responsible', 'admin'])->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

5f3d398a8594aa44b2cb7d9f8173f565d73dbe64
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-21 17:07:01 -0300

    Loga na aplicação

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
         $data = $request->validated();
-        return response()->json($data);
+
+        if(!$token = JWTAuth::attempt($data)) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        }
+
+         return response()
+            ->json(['message' => 'Login bem-sucedido'])
+            ->cookie('token', $token, 120, '/', null, false, true);
     }
 }

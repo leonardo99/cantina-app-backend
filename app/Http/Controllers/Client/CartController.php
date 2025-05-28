65b32ec79f3de3d4952e4526c0c6624e5db81e39
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-28 18:16:39 -0300

    Ações do carrinho

---
+++ app/Http/Controllers/Client/CartController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\cartItemResource;
+use App\Http\Resources\CartResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Cart;
+use App\Models\CartItems;
+use App\Models\Product;
+use Exception;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index() 
+    {
+        $user = auth()->id();
+        $cart = Cart::with('items')->where('user_id', $user)->get()->first();
+
+        if($cart && $cart->items) {
+            return cartItemResource::collection($cart->items);
+        }
+        return response()->json(["data" => []], 200);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $user = auth()->id();
+            $cart = Cart::firstOrCreate(['user_id' => $user]);
+            $product = Product::find($request->id);
+
+            if($cart && $product) {
+                $saveProduct = $cart->items()->updateOrCreate(
+                    ["product_id" => $product->id],
+                    [
+                    "price" => $product->amount,
+                    "amount" => $request->quantity,
+                    ]
+                );
+                if(!$saveProduct) {
+                    return response()->json(['error' => 'Ocorreu um erro ao tentar adicionar o item ao carrinho'], 500);
+                }
+
+                return new cartItemResource($saveProduct);
+            }
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
+    }
+
+    public function show(CartItems $cartItem)
+    {
+        try {
+            return new cartItemResource($cartItem);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
+    }
+
+    public function destroy(Cart $cart, CartItems $item)
+    {
+        try {
+            if($cart->items->count() === 0) {
+                $cart->delete();
+                return response()->json([], 204);
+            }
+
+            $userCart = $cart->where('user_id', auth()->id())->get()->first();
+
+            if($userCart->items->where("id", $item->id)->count()) {
+                $deleteItem = $item->delete();
+            }
+            
+            if(!$deleteItem) {
+                    return response()->json(['error' => 'Ocorreu um erro ao tentar remover o item do carrinho'], 500);
+            }
+
+            return response()->json(["data" => "Item removido"], 200);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductByCategoryController;
+use App\Http\Controllers\Client\CartController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\JwtMiddleware;
@@ -44,7 +46,13 @@ Route::controller(AuthController::class)
 Route::prefix('user')
 ->middleware(JwtMiddleware::class)
 ->group(function () {
+    Route::get('category/{category}', [ProductByCategoryController::class, 'index']);
     Route::get('/product', [ProductController::class, 'index']);
+    Route::get('/cart', [CartController::class, 'index']);
+    Route::post('/cart', [CartController::class, 'store']);
+    Route::get('/item/{cartItem}', [CartController::class, 'show']);
+    Route::delete('/cart/{cart}/item/{item}', [CartController::class, 'destroy']);
+
 });
 
 Route::prefix('admin')
@@ -52,6 +60,7 @@ Route::prefix('admin')
 ->group(function () {
     Route::prefix('/category')
     ->group(function() {
+        Route::get('/{category}', [ProductByCategoryController::class, 'index']);
         Route::get('/', [CategoryController::class, 'index']);
         Route::post('/', [CategoryController::class, 'store']);
     });
@@ -61,6 +70,7 @@ Route::prefix('admin')
         Route::get('/', [ProductController::class, 'index']);
         Route::post('/', [ProductController::class, 'store']);
         Route::prefix('/{product}')->group(function() {
+            Route::get('/', [ProductController::class, 'show']);
             Route::put('/update', [ProductController::class, 'update']);
             Route::delete('/delete', [ProductController::class, 'destroy']);
         });

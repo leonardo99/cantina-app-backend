2872f6a7d89d3a776612cc4846a88ecc3754e7cc
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-28 20:53:09 -0300

    Relacionamento com orders

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,8 @@ class User extends Authenticatable implements JWTSubject
     public function cart() {
         return $this->hasOne(Cart::class);
     }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
 }

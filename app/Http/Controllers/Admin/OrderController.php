d54fa63436019718dff90feb1ffa738c8e7b5039
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-28 21:07:27 -0300

    Adiciona filtros

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -5,12 +5,25 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function index() 
+    public function index(Request $request) 
     {
-        $cart = Order::paginate();
+        $cart = Order::when($request->status === "peding", function($order) {
+            return $order->where("status", "pending");
+        })
+        ->when($request->status === "preparing", function($order) {
+            return $order->where("status", "preparing");
+        })
+        ->when($request->status === "prepared", function($order) {
+            return $order->where("status", "prepared");
+        })
+        ->when($request->status === "delivered", function($order) {
+            return $order->where("status", "delivered");
+        })
+        ->paginate();
         return OrderResource::collection($cart);
     }
 }

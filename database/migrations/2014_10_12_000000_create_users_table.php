c9413d68ccdffb2b2a155edc952299a83169c9de
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 08:48:12 -0300

    remove indice unique do campo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone', 14)->nullable();
-            $table->enum('type', ['student', 'responsible', 'admin'])->unique();
+            $table->enum('type', ['student', 'responsible', 'admin']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

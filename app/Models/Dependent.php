4e380f954ce12a32eba9cbf3ba29a2e7f4c8b152
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 08:47:39 -0300

    Adiciona chaves locais e estrangeiras nos relacionamentos

--- app/Models/Dependent.php
+++ app/Models/Dependent.php
@@ -12,10 +12,10 @@ class Dependent extends Model
     protected $fillable = ["responsible_id", "user_id"];
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function responsible() {
-        return $this->belongsTo(Responsible::class);
+        return $this->belongsTo(Responsible::class, 'responsible_id', 'id');
     }
 }

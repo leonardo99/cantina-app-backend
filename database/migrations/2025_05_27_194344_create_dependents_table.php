3175b051ad5df0d6fcd188c67c8b220410d80715
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 08:48:26 -0300

    Altera tipo do campo

--- database/migrations/2025_05_27_194344_create_dependents_table.php
+++ database/migrations/2025_05_27_194344_create_dependents_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('dependents', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id")->constrained("users");
-            $table->foreignId("responsible_id")->nullable()->constrained("responsibles");
+            $table->unsignedBigInteger("responsible_id")->nullable();
             $table->timestamps();
             $table->softDeletes();
         });

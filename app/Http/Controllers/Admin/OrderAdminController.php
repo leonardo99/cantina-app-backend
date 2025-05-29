140b1d4e2db077db17c544cc8ac47f93dfdbd7d3
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 18:03:39 -0300

    Atualiza status de pedido

--- app/Http/Controllers/Admin/OrderAdminController.php
+++ app/Http/Controllers/Admin/OrderAdminController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use Exception;
 use Illuminate\Http\Request;
 
 class OrderAdminController extends Controller
 {
     public function index(Request $request) 
     {
-        $cart = Order::when($request->status === "peding", function($order) {
+        $order = Order::with('items')->when($request->status === "pending", function($order) {
             return $order->where("status", "pending");
         })
         ->when($request->status === "preparing", function($order) {
@@ -24,6 +26,18 @@ class OrderAdminController extends Controller
             return $order->where("status", "delivered");
         })
         ->paginate();
-        return OrderResource::collection($cart);
+
+        return OrderResource::collection($order);
+    }
+
+    public function update(OrderRequest $request, Order $order)
+    {
+        try {
+            $order->status = $request->status;
+            $order->save();
+            return new OrderResource($order);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
     }
 }
